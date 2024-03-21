451c3e5ec4e299e940be7034abc7bf5d07c55f79
Hiranmai Karedla <30773275+HiranmaiKaredla@users.noreply.github.com>
2024-03-20 22:40:59 -0400

    streamlit start page

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from st_functions import st_button, load_css
+from PIL import Image
+
+st.header('Hiranmai Karedla')
+
+st.info('Data Scientist with 3+ Years of Professional Experience')
